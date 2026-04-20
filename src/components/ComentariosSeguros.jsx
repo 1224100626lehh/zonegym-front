62aeb775c2f3c34209e9ad63f8079f74c7e7bbd7
Elena <1224100626.lehh@gmail.com>
2026-04-20 16:58:04 -0600

    Cambios para ejercicio 4

--- src/components/ComentariosSeguros.jsx
+++ src/components/ComentariosSeguros.jsx
@@ -9,7 +9,7 @@ function ComentariosSeguros() {
     if (!texto.trim()) return;
 
     try {
-      const res = await fetch('http://localhost:5000/api/comentarios', {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/...`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ texto })
