8b53023ed90f46a7afab762260b99ab0efc50f6c
Elena <1224100626.lehh@gmail.com>
2026-04-20 18:18:42 -0600

    eliminacion de railway

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    preview:{
+  plugins: [react(), tailwindcss()],
+  preview: {
     allowedHost: true,
     host: '0.0.0.0',
-    port: process.env.PORT || 4173,
+    port: 4173,
     allowedHosts: [
-      'zonegym-frontend-production.up.railway.app', // Tu dominio de Railway
-      'all' // Opción alternativa para permitir cualquier host
+      'zonegym-front.onrender.com',
+      'all'
     ]
   }
 });
